c2c2be5bfc5cc6882575edee6ea18d9c7ef356cc
Kaio Plandel <kaioplandel@gmail.com>
2023-06-26 04:57:16 -0300

    feat: add tab bar in screen

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -1,12 +1,63 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+import 'package:news_app/widgets/custom_tag.dart';
+import 'package:news_app/widgets/image_container.dart';
 
 class ArticleScreen extends StatelessWidget {
   const ArticleScreen({Key? key}) : super(key: key);
   static const routeName = "/article";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final article = ModalRoute.of(context)?.settings.arguments as Article;
+
+    return ImageContainer(
+        width: double.infinity,
+        imageUrl: article.imageUrl,
+        chield: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            iconTheme: IconThemeData(color: Colors.white),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          body: ListView(
+            children: [
+              _NewsHeadline(
+                article: article,
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class _NewsHeadline extends StatelessWidget {
+  const _NewsHeadline({
+    super.key,
+    required this.article,
+  });
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          CustomTag(
+              backgroundColor: Colors.grey.withAlpha(150),
+              children: Text(
+                article.category,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.white),
+              ))
+        ],
+      ),
+    );
   }
 }
--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+import 'package:news_app/screens/article_screen.dart';
+import 'package:news_app/widgets/image_container.dart';
 
 import '../widgets/bottom_nav_bar.dart';
 
@@ -11,53 +14,200 @@ class DiscoverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu),
-          color: Colors.black,
+    List<String> tabs = ["Health", "Politics", "Art", "Food", "Science"];
+
+    return DefaultTabController(
+      initialIndex: 0,
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+            color: Colors.black,
+          ),
+        ),
+        extendBodyBehindAppBar: true,
+        bottomNavigationBar: const BottomNavBar(index: 1),
+        body: ListView(
+          padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
+          children: [
+            _DiscoverNews(),
+            _CategaoriesNews(
+              categories: tabs,
+            )
+          ],
         ),
       ),
-      extendBodyBehindAppBar: true,
-      bottomNavigationBar: BottomNavBar(index: 1),
-      body: ListView(
-        padding: EdgeInsets.only(top: 100, left: 20, right: 20),
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+}
+
+class _CategaoriesNews extends StatelessWidget {
+  const _CategaoriesNews({
+    super.key,
+    required this.categories,
+  });
+
+  final List<String> categories;
+
+  @override
+  Widget build(BuildContext context) {
+    final articles = Article.articles;
+
+    return Column(
+      children: [
+        TabBar(
+          isScrollable: true,
+          indicatorColor: Colors.black,
+          tabs: categories
+              .map((tab) =>
+              Tab(
+                  icon: Text(
+                    tab,
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headlineSmall
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  )))
+              .toList(),
+        ),
+        SizedBox(
+          height: MediaQuery
+              .of(context)
+              .size
+              .height,
+          child: TabBarView(
             children: [
-              Text(
-                "Discover",
-                style: Theme.of(context).textTheme.headline4?.copyWith(
-                    color: Colors.black, fontWeight: FontWeight.w900),
-              ),
-              SizedBox(height: 5),
-              Text("News from all over the world",
-                  style: Theme.of(context).textTheme.bodySmall),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Seach",
-                  filled: true,
-                  fillColor: Colors.grey.shade200,
-                  prefixIcon: Icon(
-                    Icons.search,
+              categories
+                  .map(
+                    (tab) =>
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: categories.length,
+                      itemBuilder: ((context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(
+                                context, ArticleScreen.routeName,
+                                arguments: articles[index]);
+                          },
+                          child: Row(
+                            children: [
+                              ImageContainer(
+                                width: 80,
+                                height: 80,
+                                imageUrl: articles[index].imageUrl,
+                                chield: Container(),
+                                margin: EdgeInsets.all(10),
+                                borderRadius: 5,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      articles[index].title,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.clip,
+                                      style: Theme
+                                          .of(context)
+                                          .textTheme
+                                          .bodyLarge
+                                          ?.copyWith(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.schedule, size: 18),
+                                        SizedBox(width: 5),
+                                        Text(
+                                            "${DateTime
+                                                .now()
+                                                .difference(
+                                                articles[index].createdAt)
+                                                .inHours} hours ago",
+                                            style: TextStyle(fontSize: 12)),
+                                        SizedBox(width: 20),
+                                        Icon(Icons.visibility, size: 18),
+                                        SizedBox(width: 5),
+                                        Text("${articles[index].views} views ",
+                                            style: TextStyle(fontSize: 12)),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                    ),
+              )
+                  .toList()
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _DiscoverNews extends StatelessWidget {
+  const _DiscoverNews({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery
+          .of(context)
+          .size
+          .height * 0.25,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Discover",
+            style: Theme
+                .of(context)
+                .textTheme
+                .headline4
+                ?.copyWith(color: Colors.black, fontWeight: FontWeight.w900),
+          ),
+          const SizedBox(height: 5),
+          Text("News from all over the world",
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .bodySmall),
+          const SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+                hintText: "Seach",
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                suffix: const RotatedBox(
+                  quarterTurns: 1,
+                  child: Icon(
+                    Icons.tune,
                     color: Colors.grey,
                   ),
-                  suffix: const RotatedBox(
-                    quarterTurns: 1,
-                    child: Icon(
-                      Icons.tune,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20), borderSide: BorderSide.none)
                 ),
-              )
-            ],
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none)),
           )
         ],
       ),
